fc2297e015dc6ef9f7df75a2ddc8f4047842ad40
Valery <valera.vlasov11@mail.ru>
2023-11-06 22:27:44 +0300

    Добавьте файлы проекта.

---
+++ TetrisWPF/BlockQueue.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using TetrisWPF.Blocks;
+
+namespace TetrisWPF
+{
+    public class BlockQueue
+    {
+        private readonly Block[] blocks = new Block[]
+        {
+            new IBlock(),
+            new JBlock(),
+            new LBlock(),
+            new OBlock(),
+            new SBlock(),
+            new TBlock(),
+            new ZBlock()
+        };
+
+        private readonly Random random = new Random();
+
+        public Block NextBlock { get; private set; }
+
+        private Block RandomBlock()
+        {
+            return blocks[random.Next(blocks.Length)];
+        }
+
+        public Block GetAndUpdate()
+        {
+            Block block = NextBlock;
+
+            do
+            {
+                NextBlock = RandomBlock();
+            }
+            while (block.Id == NextBlock.Id);
+
+            return block;
+        }
+    }
+}
